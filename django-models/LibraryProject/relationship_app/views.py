b05bed75b54924da4c12b08724b58afa2db58cf8
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-21 00:14:42 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.contrib.auth.decorators import permission_required
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
 from .models import Library  # ✅ includes Library
 from .forms import BookForm  # Assuming you have a form
