4d056ad82b0e7652da9db424fe1aa35fe6d8ce20
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-20 23:40:22 +0200

    Add files via upload

---
+++ django-models/LibraryProject/relationship_app/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+class Book(models.Model):
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_add_book", "Can add book"),
+            ("can_change_book", "Can change book"),
+            ("can_delete_book", "Can delete book"),
+        ]
+
+    def __str__(self):
+        return self.title
+from django.db import models
+from django.contrib.auth.models import User
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    # Add other fields as needed
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
