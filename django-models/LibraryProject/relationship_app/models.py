ba437789147386f7b736568d3c5664416298f2df
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-21 00:29:39 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -29,3 +29,14 @@ class Librarian(models.Model):
 
     def __str__(self):
         return self.name
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    
+    ROLE_CHOICES = [
+        ('Admin', 'Admin'),
+        ('Member', 'Member'),
+    ]
+    role = models.CharField(max_length=10, choices=ROLE_CHOICES)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role}"
