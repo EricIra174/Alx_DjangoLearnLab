d289ecb31b0e79f2edeb14d5559f3f9fe595622d
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-20 23:50:41 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -1,4 +1,12 @@
 from django.db import models
+from django.contrib.auth.models import User
+
+class Author(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.user.username
 
 class Book(models.Model):
     title = models.CharField(max_length=255)
@@ -14,13 +22,10 @@ class Book(models.Model):
 
     def __str__(self):
         return self.title
-from django.db import models
-from django.contrib.auth.models import User
 
-class Author(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE)
-    # Add other fields as needed
-    bio = models.TextField(blank=True)
+class Librarian(models.Model):
+    name = models.CharField(max_length=100)
+    library = models.CharField(max_length=100)
 
     def __str__(self):
         return self.name
