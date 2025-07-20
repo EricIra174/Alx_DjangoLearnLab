77c85bc2676c0be9c6f1e1725778fb62c93622d4
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-20 23:43:49 +0200

    Update models.py

--- django-models/LibraryProject/relationship_app/models.py
+++ django-models/LibraryProject/relationship_app/models.py
@@ -17,10 +17,10 @@ class Book(models.Model):
 from django.db import models
 from django.contrib.auth.models import User
 
-class UserProfile(models.Model):
+class Author(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     # Add other fields as needed
     bio = models.TextField(blank=True)
 
     def __str__(self):
-        return self.user.username
+        return self.username
