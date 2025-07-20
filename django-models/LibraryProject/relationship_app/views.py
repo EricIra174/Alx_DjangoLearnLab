5c9a5a3200fd777a0a2696b538739334ce48c5ec
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-21 00:34:55 +0200

    Update views.py

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import user_passes_test
+from .models import UserProfile
 from django.contrib.auth import login
 from django.contrib.auth.forms import UserCreationForm
 from django.views.generic.detail import DetailView
