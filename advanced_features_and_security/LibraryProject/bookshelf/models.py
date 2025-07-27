7e48ca1fcaebf73360714be7582b5510720fc01f
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-27 22:16:54 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -37,3 +37,19 @@ class CustomUser(AbstractUser):
 
     def __str__(self):
         return self.username
+
+# Book model with custom permissions
+class Book(models.Model):
+    title = models.CharField(max_length=200)
+    author = models.CharField(max_length=100)
+    published_date = models.DateField()
+
+    class Meta:
+        permissions = [
+            ("can_create", "Can create a book"),
+            ("can_delete", "Can delete a book"),
+        ]
+
+    def __str__(self):
+        return self.title
+
