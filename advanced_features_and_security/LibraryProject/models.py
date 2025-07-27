e66df40fa1586681ddfa603945cb2667596d05c0
EricIra174 <181249888+EricIra174@users.noreply.github.com>
2025-07-27 20:54:17 +0200

    Update models.py

--- advanced_features_and_security/LibraryProject/models.py
+++ advanced_features_and_security/LibraryProject/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 # ✅ Custom User model
-class CustomUser(AbstractUser):
+class CustomUser(AbstractUser): "date_of_birth", "profile_photo"
     "date_of_birth" = models.DateField(null=True, blank=True)
     "profile_photo" = models.ImageField(upload_to="profile_photos/", null=True, blank=True)
 
